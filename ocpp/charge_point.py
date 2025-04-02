f5d7f5be67b65fd481b3be29691036ab4eea4210
Rafael Paz <rafael@jupiter.com.br>
2025-04-02 17:12:56 -0300

    Retorno nas response

--- ocpp/charge_point.py
+++ ocpp/charge_point.py
@@ -233,8 +233,7 @@ class ChargePoint:
 
         # A queue used to pass CallResults and CallErrors from
         # the self.serve() task to the self.call() task.
-        self._response_queue = {}
-        # self._response_queue = asyncio.Queue()
+        self._response_queue = asyncio.Queue()
 
         # Function used to generate unique ids for CALLs. By default
         # uuid.uuid4() is used, but it can be changed. This is meant primarily
@@ -281,8 +280,7 @@ class ChargePoint:
 
         elif msg.message_type_id in [MessageType.CallResult, MessageType.CallError]:
             self.logger.warning(f"Messagem teste: {msg}")
-            # self._response_queue.put_nowait(msg)
-            self._response_queue[msg.unique_id] = msg
+            self._response_queue.put_nowait(msg)
 
     async def _handle_call(self, msg):
         """
@@ -458,7 +456,8 @@ class ChargePoint:
         wait_until = time.time() + timeout
         try:
             # Wait for response of the Call message.
-            response = await asyncio.wait_for(self._response_queue[unique_id], timeout)
+            response = await asyncio.wait_for(self._response_queue.get(), timeout)
+            self.logger.error("Retorno response: %s", response)
         except asyncio.TimeoutError:
             raise
 
